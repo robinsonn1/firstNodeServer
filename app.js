3d49537532efc6240e67dad1a77d0baae4b38f51
Robinson <robinsonnavarro@gmail.com>
2022-10-10 17:19:46 -0500

    preparando para subir imagen 2

--- app.js
+++ app.js
@@ -5,7 +5,9 @@ const app = express()
 app.get('/', function(req, res){
     res.send('hola mundo')
 })
-app.post('/image', function(req, res){
+app.post('/imagen', function(req, res){
+    const body = req.body
+    console.log(body)
     res.send('hola mundo desde el post')
 })
 
