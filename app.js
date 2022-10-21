5156bbdef8181729382ff61998aaf7a83133efbb
Robinson <robinsonnavarro@gmail.com>
2022-10-21 16:38:21 -0500

    cambios listos para resized

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const multer = require('multer')
+const sharp = require('sharp')
+const fs = require('fs') //manejo de archivos usado para crear un archivo con el buffer
 
 const storageStrategy = multer.memoryStorage()
 const upload = multer({ storage: storageStrategy })
@@ -10,11 +12,18 @@ app.use(express.json())//funcion que se ejecuta antes que la info llegue a las r
 app.get('/', function(req, res){
     res.send('hola mundo 2')
 })
-app.post('/imagen', upload.single('imagen'), function(req, res){
+app.post('/imagen', upload.single('imagen'), async function(req, res){ //debe ser async por el await y son procesos que deben terminar en un tiempo
     //const body = req.body
     const imagen = req.file
-    console.log(imagen)
-    res.send('hola mundo desde el post')
+    const processedImage = sharp(imagen.buffer)
+    const resizedImage = processedImage.resize(800,600, {
+        fit:  "contain",
+        background: "#FFF"
+    }) //ctrl+space para ver las opciones
+    const resizedImageBuffer = await resizedImage.toBuffer() //devuelve una promesa, necesitamos un await para que se finalice el buffer de datos
+    fs.writeFileSync('nuevaruta/prueba.png', resizedImageBuffer) //la carpeta ya debe estar creada
+    console.log(resizedImageBuffer)
+    res.send({ resizedImage: resizedImageBuffer})
 })
 
 const PORT = process.env.PORT||3000 //Variable de entorno PORT
